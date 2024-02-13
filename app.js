e9143ea42c41b61d4d40aca3b2ffb29643ed4000
imranaly <139737256+rahu-ss@users.noreply.github.com>
2024-02-13 17:10:44 +0530

    added new style

--- app.js
+++ app.js
@@ -25,3 +25,39 @@ document.addEventListener('DOMContentLoaded',function(){
         
     })
 })
+
+
+
+
+
+
+
+
+
+// emergency
+
+
+let emergency = document.querySelector('.Emergency');
+let isRed = false;
+
+setInterval(() => {
+    if (isRed) {
+        emergency.style.backgroundColor = ""; // Set it back to default (which is usually transparent)
+    } else {
+        emergency.style.backgroundColor = "red";
+    }
+    isRed = !isRed; // Toggle the state
+}, 100);
+
+
+//close emergency
+
+
+
+let close=document.getElementById('close');
+
+close.addEventListener('click',function () {
+
+    emergency.remove()
+    
+})
